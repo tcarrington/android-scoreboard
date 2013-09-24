74801420d414580779177c5f3acec5a77406c9ea
Taylor Carrington <taylor.carrington91@gmail.com>
2013-09-23 22:49:08 -0700

    progress on adding to a listview

--- src/tournament/brackets/QuickMatchActivity.java
+++ src/tournament/brackets/QuickMatchActivity.java
@@ -1,20 +1,39 @@
 package tournament.brackets;
 
 import android.app.Activity;
+import android.app.ListActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.ArrayAdapter;
+
+import java.util.ArrayList;
 
 
-public class QuickMatchActivity extends Activity {
+public class QuickMatchActivity extends ListActivity {
+    ArrayList<String> playerListItems = new ArrayList<String>();
+
+    ArrayAdapter<String> adapter;
+
+    int clickCounter = 0;
+
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quick_match);
+        adapter = new ArrayAdapter<String>(this, android.R.id.list, playerListItems);
+        setListAdapter(adapter);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.create_tournament_settings, menu);
+        //getMenuInflater().inflate(R.menu.create_tournament_settings, menu);
         return true;
+    }
+
+    public void addPlayerItems(View view) {
+        playerListItems.add("Clicked test");
+        clickCounter++;
+        adapter.notifyDataSetChanged();
     }
 }
