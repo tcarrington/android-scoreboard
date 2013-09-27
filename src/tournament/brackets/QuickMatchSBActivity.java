07513ca38f0c19ad81841e6f1fe151f2619b49a9
Taylor Carrington <taylor.carrington91@gmail.com>
2013-09-27 14:33:08 -0700

    added new activity for actual quickmatch scoreboard

---
+++ src/tournament/brackets/QuickMatchSBActivity.java
@@ -0,0 +1,13 @@
+package tournament.brackets;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Taylor on 9/27/13.
+ */
+public class QuickMatchSBActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
