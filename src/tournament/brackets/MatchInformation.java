bf3ed69afceaf01c085e9885630e8c39cefae68b
Taylor Carrington <taylor.carrington91@gmail.com>
2013-10-15 22:15:45 -0700

    new java class MatchInformation

---
+++ src/tournament/brackets/MatchInformation.java
@@ -0,0 +1,17 @@
+package tournament.brackets;
+
+/**
+ * Created by Taylor on 10/15/13.
+ */
+public class MatchInformation {
+    //this will hold methods and primatives for matches, should only be inherited from
+
+    //timer
+    //winner, loser
+    //date
+    //match type
+    //number of players
+
+    //create entry for database
+    
+}
