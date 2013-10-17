73183ea4b2beeffc059b1f5c2c11deb5bbab0969
Taylor Carrington <taylor.carrington91@gmail.com>
2013-10-16 23:12:29 -0700

    working to add date information

--- src/tournament/brackets/MatchInformation.java
+++ src/tournament/brackets/MatchInformation.java
@@ -1,5 +1,7 @@
 package tournament.brackets;
 
+import java.util.Calendar;
+
 /**
  * Created by Taylor on 10/15/13.
  */
@@ -7,11 +9,16 @@ public class MatchInformation {
     //this will hold methods and primatives for matches, should only be inherited from
 
     //timer
+    Calendar c = Calendar.getInstance();
+
+    public Calendar getDate() {
+        return c;
+    }
     //winner, loser
     //date
     //match type
     //number of players
 
     //create entry for database
-    
+
 }
