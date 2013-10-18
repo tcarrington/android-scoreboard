758aee25db98a160e826e427155358849f038dd3
Taylor Carrington <taylor.carrington91@gmail.com>
2013-10-17 23:02:16 -0700

    added new files for soccer activity, removed old 'sb' activity

---
+++ src/tournament/brackets/SoccerActivity.java
@@ -0,0 +1,14 @@
+package tournament.brackets;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Taylor on 10/17/13.
+ */
+public class SoccerActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+    //first sport built
+}
