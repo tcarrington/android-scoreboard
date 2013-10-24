58f61eb46803f2214123a83d6afef949ca176092
Taylor Carrington <taylor.carrington91@gmail.com>
2013-10-23 23:14:55 -0700

    added football activity

---
+++ src/tournament/brackets/FootballActivity.java
@@ -0,0 +1,13 @@
+package tournament.brackets;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Taylor on 10/23/13.
+ */
+public class FootballActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
