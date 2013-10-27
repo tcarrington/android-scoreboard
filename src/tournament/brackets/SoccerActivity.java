052764c753f82df9790d6b519f95e0637c6ba5bd
Taylor Carrington <taylor.carrington91@gmail.com>
2013-10-26 22:02:48 -0700

    have score increment working (team 1)

--- src/tournament/brackets/SoccerActivity.java
+++ src/tournament/brackets/SoccerActivity.java
@@ -3,19 +3,26 @@ package tournament.brackets;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 /**
  * Created by Taylor on 10/17/13.
  */
 public class SoccerActivity extends Activity {
+
+    MatchInformation soccerInformation = new MatchInformation();
+
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        MatchInformation matchInformation = new MatchInformation();
         setContentView(R.layout.activity_soccer);
     }
 
 
     public void team1ScoreIncrement(View view){
-
+        soccerInformation.addPoint(1);
+        int score = soccerInformation.getTeam1Score();
+        TextView textView =  (TextView) findViewById(R.id.team_1_score);
+        textView.setText(String.valueOf(score));
     }
+
 }
