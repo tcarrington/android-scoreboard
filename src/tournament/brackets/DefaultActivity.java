eefea03b9631d885ec088e64c05b1702ccaf4db4
Taylor Carrington <taylor.carrington91@gmail.com>
2013-11-03 22:00:26 -0800

    added default files to project

---
+++ src/tournament/brackets/DefaultActivity.java
@@ -0,0 +1,13 @@
+package tournament.brackets;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Taylor on 11/3/13.
+ */
+public class DefaultActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
